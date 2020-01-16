52a265468fc287f286a6525fbfb511402b580b5d
Nikolay Vetrov <energok3@gmail.com>
2020-01-16 10:27:31 +0200

    Unit_6 lesson

---
+++ src/footer/Footer.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './Footer.css';
+
+
+class Footer extends React.Component {
+    render() {
+        return (
+            <footer className="section footer-classic context-dark dg-image"
+                style={{ background: "#2d3246", color: "#fff" }}>
+                Copyright
+            </footer>
+        )
+    }
+}
+export default Footer;
---
+++ src/main/Main.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import './Main.css';
+
+
+class Main extends React.Component {
+    render() {
+        return (
+            <div className="container">
+                <main>
+                    <article>
+                        <h1>Article 1</h1>
+                        <h2>Article 2</h2>
+                        <p>
+                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Illo, iusto! Neque, voluptatibus placeat numquam pariatur sit molestias nisi maxime debitis ex corrupti nobis. Iure ad sed quam nisi, placeat deserunt.
+                    </p>
+                        <p>
+                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Illo, iusto! Neque, voluptatibus placeat numquam pariatur sit molestias nisi maxime debitis ex corrupti nobis. Iure ad sed quam nisi, placeat deserunt.
+                    </p>
+                    </article>
+                </main>
+            </div>
+        )
+    }
+}
+export default Main;
